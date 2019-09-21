2aed364b1bb6a33a29c94bb42c93ed9dc581bc03
Valentin Markin <jobedano@gmail.com>
2019-09-21 17:08:13 +0400

    models created. create table applied

---
+++ db/migrate/20190921125722_create_answers.rb
@@ -0,0 +1,11 @@
+class CreateAnswers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :answers do |t|
+      t.integer :choice
+      t.integer :test_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
