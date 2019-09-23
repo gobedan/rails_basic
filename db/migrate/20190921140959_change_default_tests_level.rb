d63506b1050caa540289d5cc047ea9ebc4f4810f
Valentin Markin <jobedano@gmail.com>
2019-09-23 15:51:27 +0400

    update 1

--- db/migrate/20190921140959_change_default_tests_level.rb
+++ db/migrate/20190921140959_change_default_tests_level.rb
@@ -2,4 +2,11 @@ class ChangeDefaultTestsLevel < ActiveRecord::Migration[6.0]
   def change
     change_column_default(:tests, :level, from: nil, to: 1)
   end
+  def up 
+    change_column_null(:tests, :level,  false)
+  end
+
+  def down
+    change_column_null(:tests, :level,  true)
+  end
 end
--- db/migrate/20190921141307_change_default_answers_correct.rb
+++ db/migrate/20190921141307_change_default_answers_correct.rb
@@ -2,4 +2,11 @@ class ChangeDefaultAnswersCorrect < ActiveRecord::Migration[6.0]
   def change
     change_column_default(:answers, :correct, from: nil, to: false)
   end
+  def up 
+    change_column_null(:answers, :correct,  false)
+  end
+
+  def down
+    change_column_null(:answers, :correct,  true)
+  end
 end
