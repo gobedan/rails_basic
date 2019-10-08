fc15af224ac466955b9159658862134dd5cd0565
Valentin Markin <jobedano@gmail.com>
2019-10-09 02:04:46 +0400

    update 2 hotfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   def get_tests_by_level(level)
     Test.joins('JOIN user_tests AS ut ON tests.id = ut.test_id')
-        .joins('JOIN users ON ut.user_id = users.id')
-        .where('level = ? AND users.id = ?', level, id)
+        .where('level = ? AND ut.user_id = ?', level, id)
   end
 end
