f4899ae8b9ac9870e999294a6cd436396d957643
Valentin Markin <jobedano@gmail.com>
2019-10-09 11:13:15 +0400

    update 3

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,12 +1,7 @@
 class Test < ApplicationRecord
   def self.get_by_category(category)
-    result = [] 
     Test.joins('JOIN categories as cat ON category_id = cat.id')
         .where(cat: {title: category})
-        .to_a
-        .each do |test| 
-          result.push(test.title)
-        end
-    result     
+        .pluck(:title)         
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def get_tests_by_level(level)
-    Test.joins('JOIN user_tests AS ut ON tests.id = ut.test_id')
-        .where('level = ? AND ut.user_id = ?', level, id)
+    Test.joins('JOIN user_tests ON tests.id = user_tests.test_id')
+        .where(level: level, user_tests: {user_id: id}) 
   end
 end
