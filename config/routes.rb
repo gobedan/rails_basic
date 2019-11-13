fc480858d1d13dc8d16ab33f42e5e41fb5c210a7
Valentin Markin <jobedano@gmail.com>
2019-11-13 13:15:41 +0400

    task8 update2.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'tests#index'
   resources :tests do
-    resources :questions, only: %i[show edit update create new destroy], 
-                          shallow: true
+    resources :questions, except: :index, shallow: true
   end
 end
