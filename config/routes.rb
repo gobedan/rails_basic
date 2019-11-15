8d986a767c3a704400a553c63d612ac5d6c5e44a
Valentin Markin <jobedano@gmail.com>
2019-11-15 14:50:31 +0400

    task9.VOD scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'tests#index'
   resources :tests do
-    resources :questions, except: :index, shallow: true
+    resources :questions, except: :index, shallow: true do
+      resources :answers, shallow: true
+    end
   end
 end
