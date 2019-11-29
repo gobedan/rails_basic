3b3d9bd1a3ab3c3b884bf3b65b6553629a5d58d4
Valentin Markin <jobedano@gmail.com>
2019-11-29 08:42:13 +0400

    task10 update2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def github_url(author, repo)
     "https://github.com/#{author}/#{repo}"
   end
+
+  def flash_messages 
+    content_tag :div, class: "flash-messages" do 
+      flash.each do |key, message| 
+        concat(content_tag :p, message, class: "flash #{key}")
+      end
+    end
+  end
 end
