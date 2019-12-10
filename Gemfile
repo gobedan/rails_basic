dfe405a079a25d96fc49595acf0671999d263f95
Valentin Markin <jobedano@gmail.com>
2019-12-10 23:00:46 +0400

    task13 complete

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'rails-i18n', '~> 6.0.0'
 # UI
 gem 'bootstrap', '~> 4.0.0'
 gem 'jquery-rails'
+gem 'octicons'
+gem 'octicons_helper'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,10 @@ module ApplicationHelper
   def flash_messages 
     content_tag :div, class: "flash-messages" do 
       flash.each do |key, message| 
-        concat(content_tag :p, message, class: "flash #{key}")
+        # насколько приемлимы условные интерполяции? 
+        concat(content_tag :p, message, 
+                               class: "flash-#{key} alert alert-#{"danger" if key == "alert"}#{"success" if key == "notice"}"
+        )
       end
     end
   end
