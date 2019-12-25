7288dd16c3d1c24bbb7997ed4a3e6873c02a843f
Valentin Markin <jobedano@gmail.com>
2019-12-25 14:27:42 +0400

    task15 complete

---
+++ app/javascript/packs/utilities/confirmation_check.js
@@ -0,0 +1,18 @@
+document.addEventListener('turbolinks:load', function(){
+  var confirmation = document.querySelector('#user_password_confirmation');
+  if (confirmation) { confirmation.addEventListener('input', checkPasswordConfirmation); }
+
+  function checkPasswordConfirmation() {
+    if (comparePasswordFields()) {
+      this.parentNode.querySelector('.octicon-issue-opened').classList.add('hide');
+      this.parentNode.querySelector('.octicon-issue-closed').classList.remove('hide');
+    } else {
+      this.parentNode.querySelector('.octicon-issue-closed').classList.add('hide');
+      this.parentNode.querySelector('.octicon-issue-opened').classList.remove('hide');
+    }
+  }
+
+  function comparePasswordFields() {
+    return document.querySelector('#user_password').value == document.querySelector('#user_password_confirmation').value;
+  }
+})
--- app/javascript/packs/utilities/sorting.js
+++ app/javascript/packs/utilities/sorting.js
@@ -1,13 +1,22 @@
 document.addEventListener('turbolinks:load', function(){
   // нормально что в скринкасте везде var а не let ?
   var control = document.querySelector('.sort-by-title');
-  control.addEventListener('click', sortTestsByTitle);
+  if (control) { control.addEventListener('click', sortTestsByTitle); }
 
   function sortTestsByTitle() {
     // Насколько корректный нейминг? В тч css классов
     var testsBlock = document.querySelector('.tests-block');
     var testContainers = testsBlock.querySelectorAll('.test-container');
-    var sortedContainers = Array.from(testContainers).sort(compareContainers);
+    var sortedContainers = Array.from(testContainers);
+    if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
+      sortedContainers.sort(compareContainersAsc);
+      this.querySelector('.octicon-arrow-up').classList.remove('hide');
+      this.querySelector('.octicon-arrow-down').classList.add('hide');
+    } else {
+      sortedContainers.sort(compareContainersDesc);
+      this.querySelector('.octicon-arrow-down').classList.remove('hide');
+      this.querySelector('.octicon-arrow-up').classList.add('hide');
+    }
     var sortedBlock = document.createElement('div'); 
     sortedBlock.classList = testsBlock.classList;
     for (var i = 0; i < sortedContainers.length; i++) {
@@ -16,11 +25,19 @@ document.addEventListener('turbolinks:load', function(){
     testsBlock.parentNode.replaceChild(sortedBlock, testsBlock);
   }
 
-  function compareContainers(div1, div2){
+  function compareContainersAsc(div1, div2) {
     var testTitle1 = div1.querySelector('p').textContent;
     var testTitle2 = div2.querySelector('p').textContent;
     if (testTitle1 < testTitle2) { return -1; }
     if (testTitle1 > testTitle2) { return 1; }
+    return 0;
+  }
+
+  function compareContainersDesc(div1, div2) {
+    var testTitle1 = div1.querySelector('p').textContent;
+    var testTitle2 = div2.querySelector('p').textContent;
+    if (testTitle1 < testTitle2) { return 1; }
+    if (testTitle1 > testTitle2) { return -1; }
     return 0;
   }
 
