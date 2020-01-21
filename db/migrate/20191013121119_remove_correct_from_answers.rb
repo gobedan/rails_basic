f93b6260706f9d882b95505707f16167811430b4
Valentin Markin <jobedano@gmail.com>
2020-01-21 11:57:55 +0400

    task17 pre2

--- db/migrate/20191013121119_remove_correct_from_answers.rb
+++ db/migrate/20191013121119_remove_correct_from_answers.rb
@@ -1,8 +1,5 @@
 class RemoveCorrectFromAnswers < ActiveRecord::Migration[6.0]
   def change
-
     remove_column :answers, :correct, :boolean
-    remove_column :questions, :correct, :boolean
-
   end
 end
